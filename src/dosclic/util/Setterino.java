024cb82679b88b5dcf486f2c8233187e8afa9d08
Davide <davidesavarro98@gmail.com>
2018-11-02 12:26:23 +0100

    Finished but hard coded

--- src/dosclic/util/Setterino.java
+++ src/dosclic/util/Setterino.java
@@ -3,14 +3,18 @@ package dosclic.util;
 import javax.swing.SwingUtilities;
 
 public class Setterino {
+	public static int[] setArr = new int[] {0x7E, 0x30, 0x6D, 0x79, 0x33, 0x5B, 0x5F, 0x70, 0x7F, 0x7B, 0x77, 0x1F, 0x4E, 0x3D, 0x4F, 0x47};
 	public static int set;
 	
 	public static void main(String a[]) {
-		set = 0x7E;
+		set = setArr[0];
 		ShapeTest gui = new ShapeTest();
 		waiterino(1000);
-		set = 0x30;
-		SwingUtilities.updateComponentTreeUI(gui);
+		for(int i=1;i<16;i++) {
+			set = setArr[i];
+			SwingUtilities.updateComponentTreeUI(gui);
+			waiterino(1000);
+		}
 	}
 	private static void waiterino(int n) {
 		try
--- src/dosclic/util/ShapeTest.java
+++ src/dosclic/util/ShapeTest.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ShapeTest extends JFrame {
+	private static final long serialVersionUID = 1L;
+
 	public ShapeTest(){
 		setSize(400,400);
 		setTitle("Seven Segment");
@@ -20,30 +22,72 @@ public class ShapeTest extends JFrame {
 			// A
 			g.fill3DRect(50, 50, 80, 10, true);
 		}
+		else {
+			g.setColor(new Color(20,20,20,40));
+			g.fill3DRect(50, 50, 80, 10, false);
+			g.setColor(Color.RED);
+		}
+		
 		if((Setterino.set >> 5 & 1) == 1) {
 			// B
 			g.fill3DRect(130, 60, 10, 80, true);
 		}
+		else {
+			g.setColor(new Color(20,20,20,40));
+			g.fill3DRect(130, 60, 10, 80, false);
+			g.setColor(Color.RED);
+		}
+		
 		if((Setterino.set >> 4 & 1) == 1) {
 			// C
 			g.fill3DRect(130, 150, 10, 80, true);
 		}
+		else {
+			g.setColor(new Color(20,20,20,40));
+			g.fill3DRect(130, 150, 10, 80, false);
+			g.setColor(Color.RED);
+		}
+
 		if((Setterino.set >> 3 & 1) == 1) {
 			// D
 			g.fill3DRect(50, 230, 80, 10, true);
 		}
+		else {
+			g.setColor(new Color(20,20,20,40));
+			g.fill3DRect(50, 230, 80, 10, false);
+			g.setColor(Color.RED);
+		}
+
 		if((Setterino.set >> 2 & 1) == 1) {
 			// E
 			g.fill3DRect(40, 150, 10, 80, true);
 		}
+		else {
+			g.setColor(new Color(20,20,20,40));
+			g.fill3DRect(40, 150, 10, 80, false);
+			g.setColor(Color.RED);
+		}
+
 		if((Setterino.set >> 1 & 1) == 1) {
 			// F
 			g.fill3DRect(40, 60, 10, 80, true);
 		}
+		else {
+			g.setColor(new Color(20,20,20,40));
+			g.fill3DRect(40, 60, 10, 80, false);
+			g.setColor(Color.RED);
+		}
+
 		if((Setterino.set >> 0 & 1) == 1) {
 			// G
 			g.fill3DRect(50, 140, 80, 10, true);
 		}
+		else {
+			g.setColor(new Color(20,20,20,40));
+			g.fill3DRect(50, 140, 80, 10, false);
+			g.setColor(Color.RED);
+		}
+	
 	}
 
 }
